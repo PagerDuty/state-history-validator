49727a1a1ac9fa95c55ccf4c93b39ea5ab8ab4a8
Daniel Ge <daniel@pagerduty.com>
2012-07-12 14:29:47 -0700

    Remove ALL the things

--- lib/state_history_validator.rb
+++
@@ -1,5 +0,0 @@
-require 'active_model/validations/state_history_validator'
-require 'active_model/validations/state_history_entry_validator'
-
-module StateHistoryValidator
-end
--- test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-require 'shoulda'
-require 'mocha'
-require 'mocha/integration/test_unit'
-require 'active_support/core_ext'
-require 'active_model'
-
-require 'state_history_validator'
